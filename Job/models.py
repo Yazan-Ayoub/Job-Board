65d7b05ae3af56274f3e5682dadced25274b1545
yazan <yazan.ayoub.bee@outlook.com>
2021-12-15 20:24:22 +0100

    add model

--- Job/models.py
+++ Job/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class job(models.Model):
+ title=models.CharField(max_length=50)
+ description=models.TextField(max_length=250)
+ published_at=models.DateTimeField(auto_now=True)
+ vacancy=models.IntegerField(default=1)
+ salary=models.IntegerField(default=0)
+
+def __str__(self):
+    return self.title
